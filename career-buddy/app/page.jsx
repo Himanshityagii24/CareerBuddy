d094bd9393b3f7741f660ed6f7dbe3bdaf2101d2
Himanshityagii24 <tyagihimanshi241@gmail.com>
2025-01-28 03:55:36 +0530

    navbar done

---
+++ career-buddy/app/page.jsx
@@ -0,0 +1,12 @@
+import ShowcaseNavbar from "@/components/showcase-navbar";
+import Footer from "@/components/footer";
+
+
+export default function Home() {
+  return (
+    <div>
+    <ShowcaseNavbar/>
+    <Footer/>
+    </div>
+  );
+}
