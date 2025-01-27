ac5b3299b41943046a3ad38d759593e43ce18fd9
Himanshityagii24 <tyagihimanshi241@gmail.com>
2025-01-28 04:04:23 +0530

    Footer done

--- career-buddy/components/footer.jsx
+++ career-buddy/components/footer.jsx
@@ -7,21 +7,21 @@ const Footer = () => {
       <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
         <h1 className="text-3xl md:text-5xl font-medium ">
           <Image
-            src={"/logo/logo.webp"}
+            src={"/assets/Logo-career.png"}
             width={10000}
             height={10000}
             className="w-40"
             alt="image"
           />{" "}
         </h1>
-        <p className="text-left  text-xl  text-gray-900">853-498-2098</p>
+        <p className="text-left  text-xl  text-gray-900">765-786-8862</p>
         <p className="text-left  text-xl  text-gray-900">
-          admin@prospera_career.in
+          admin@career_buddy.in
         </p>
       </div>
 
       <div className="flex md:justify-center gap-x-4 mt-10">
-        © 2024 Prospera. All Rights Reserved.
+        © 2025 Career buddy.All Rights Reserved.
         <Link href="/" className="text-blue-900">
           Privacy Policy
         </Link>
